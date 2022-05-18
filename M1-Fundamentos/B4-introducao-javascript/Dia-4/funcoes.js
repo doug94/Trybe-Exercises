75efecc672a54aa129b06a912086bc650a5951c7
Douglas Alves <douglas_dca@hotmail.com>
2022-05-17 23:32:53 -0300

    Exercícios de fixação de funções concluídos

--- M1-Fundamentos/B4-introducao-javascript/Dia-4/funcoes.js
+++ M1-Fundamentos/B4-introducao-javascript/Dia-4/funcoes.js
@@ -50,39 +50,42 @@ function positivoOuNegativo(numero) {
 }
 
 //5 -  Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
-const anguloA = 40;
-const anguloB = 60;
-const anguloC = 80;
-if (anguloA < 0 || anguloB < 0 || anguloC < 0){
-    console.log("Um ou mais ângulos é inválido");
-}
-else if(anguloA + anguloB + anguloC === 180) {
-    console.log(true);
-}
-else {
-    console.log(false);
+function calculaAngulo(anguloA, anguloB, anguloC) {
+    if (anguloA < 0 || anguloB < 0 || anguloC < 0){
+        return "Um ou mais ângulos é inválido";
+    }
+    else if(anguloA + anguloB + anguloC === 180) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 //6 - Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 function movimentosPecaXadrez(peca) {
-    switch (peca) {
-        case "Rei".toLowerCase():
-            return "Vertical", "Horizontal", "Diagonal", "Uma casa por vez";
+    pecaLowerCase = peca.toLowerCase();
+    let movimentos = ["Vertical", "Horizontal", "Diagonal", "Movimento em L"];
+    let casas = ["Uma casa por vez", "Quantas casas desejar", "Duas casas horizontais e uma vertical, ou uma casa horizontal e duas verticais"];
+    let direcoes = ["Apenas para frente", "Qualquer direção"];
+    switch (pecaLowerCase) {
+        case "rei":
+            return movimentos[0] + ", " + movimentos[1] + ", " + movimentos[2] + ", " + casas[0] + ", " + direcoes[1];
     
-        case "Rainha".toLowerCase():
-            return "Vertical, Horizontal", "Diagonal", "Quantas casas desejar";
+        case "rainha":
+            return movimentos[0] + ", " + movimentos[1] + ", " + movimentos[2] + ", " + casas[1] + ", " + direcoes[1];
     
-        case "Bispo".toLowerCase():
-            return "Diagonal", "Quantas casas desejar";
+        case "bispo":
+            return movimentos[2] + ", " + casas[1] + ", " + direcoes[1];
     
-        case "Cavalo".toLowerCase():
-            return "Movimento em L", "Duas casas horizontais e uma vertical, ou uma casa horizontal e duas verticais";
+        case "cavalo":
+            return movimentos[3] + ", " + casas[2] + ", " + direcoes[1];
     
-        case "Torre".toLowerCase():
-            return "Vertical", "Horizontal", "Quantas casas desejar";
+        case "torre":
+            return movimentos[0] + ", " + movimentos[1] + ", " + casas[1] + ", " + direcoes[1];
     
-        case "Peão".toLowerCase():
-            return "Vertical", "Uma casa por vez", "Apenas para frente";
+        case "peão":
+            return movimentos[0] + ", " + casas[0] + ", " + direcoes[0];
     
         default:
             return "Peça inválida";
@@ -90,35 +93,29 @@ function movimentosPecaXadrez(peca) {
 }
 
 //7 - Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F.
-let nota = 83;
-switch (true) {
-    case (nota >= 90 && nota <= 100):
-        console.log("A");
-        break;
+function converteNotaEmConceito(nota) {
+    switch (true) {
+        case (nota >= 90 && nota <= 100):
+            return "A";
+        
+        case (nota >= 80 && nota < 90):
+            return "B";
     
-    case (nota >= 80 && nota < 90):
-        console.log("B");
-        break;
-
-    case (nota >= 70 && nota < 80):
-        console.log("C");
-        break;
-
-    case (nota >= 60 && nota < 70):
-        console.log("D");
-        break;
-
-    case (nota >= 50 && nota < 60):
-        console.log("E");
-        break;
-
-    case (nota >= 0 && nota < 50):
-        console.log("F");
-        break;
-
-    default:
-        console.log("Nota inválida");
-        break;
+        case (nota >= 70 && nota < 80):
+            return "C";
+    
+        case (nota >= 60 && nota < 70):
+            return "D";
+    
+        case (nota >= 50 && nota < 60):
+            return "E";
+    
+        case (nota >= 0 && nota < 50):
+            return "F";
+    
+        default:
+            return "Nota inválida";
+    }
 }
 
 
@@ -143,69 +140,69 @@ function haPeloMenosUmImpar(n1, n2, n3) {
 }
 
 //10 - Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
-const custo = 500;
-const valor = 1000;
-let lucro = 0;
-if (custo < 0 || valor < 0) {
-    console.log("Valor ou custo inválido!");
-}
-else {
-    const imposto = custo * 0.2;
-    const custoTotal = custo + imposto;
-    lucro = valor - custoTotal;
-    console.log(lucro);
+function calculaLucro(custo, valor) {
+    let lucro = 0;
+    if (custo < 0 || valor < 0) {
+        return "Valor ou custo inválido!";
+    }
+    else {
+        const imposto = custo * 0.2;
+        const custoTotal = custo + imposto;
+        lucro = valor - custoTotal;
+        return lucro;
+    }
 }
 
 //11 - Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
-const salarioBruto = 15000;
-let salarioBase;
-let salarioLiquido;
-let inss;
-let ir;
-let deducao;
-if (salarioBruto <= 1556.94) {
-    inss = 0.08;
-} 
-else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
-    inss = 0.09;
+function calculaSalarioLiquido(salarioBruto) {
+    let salarioBase;
+    let salarioLiquido;
+    let inss;
+    let ir;
+    let deducao;
+    if (salarioBruto <= 1556.94) {
+        inss = 0.08;
+    } 
+    else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+        inss = 0.09;
+    }
+    else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+        inss = 0.11;
+    }
+    else if (salarioBruto > 5189.92) {
+        inss = 570.88;
+    }
+    if (inss === 570.88) {
+        salarioBase = salarioBruto - inss;
+    }
+    else {
+        salarioBase = salarioBruto - (salarioBruto * inss);
+    }
+    if (salarioBase <= 1903.98) {
+        ir = 0;
+        deducao = 0;
+    }
+    else if (salarioBase >= 1903.99 && salarioBase <= 2826.55) {
+        ir = 0.075;
+        deducao = 142.80;
+    }
+    else if (salarioBase >= 2826.56 && salarioBase <= 3751.05) {
+        ir = 0.15;
+        deducao = 354.80;
+    }
+    else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+        ir = 0.225;
+        deducao = 636.13;
+    }
+    else if (salarioBase > 4664.68) {
+        ir = 0.275;
+        deducao = 869.36;
+    }
+    if (ir != 0) {
+        salarioLiquido = salarioBase - (salarioBase * ir) + deducao;
+    }
+    else {
+        salarioLiquido = salarioBase;
+    }
+    return salarioLiquido;
 }
-else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
-    inss = 0.11;
-}
-else if (salarioBruto > 5189.92) {
-    inss = 570.88;
-}
-if (inss === 570.88) {
-    salarioBase = salarioBruto - inss;
-}
-else {
-    salarioBase = salarioBruto - (salarioBruto * inss);
-}
-if (salarioBase <= 1903.98) {
-    ir = 0;
-    deducao = 0;
-}
-else if (salarioBase >= 1903.99 && salarioBase <= 2826.55) {
-    ir = 0.075;
-    deducao = 142.80;
-}
-else if (salarioBase >= 2826.56 && salarioBase <= 3751.05) {
-    ir = 0.15;
-    deducao = 354.80;
-}
-else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
-    ir = 0.225;
-    deducao = 636.13;
-}
-else if (salarioBase > 4664.68) {
-    ir = 0.275;
-    deducao = 869.36;
-}
-if (ir != 0) {
-    salarioLiquido = salarioBase - (salarioBase * ir) + deducao;
-}
-else {
-    salarioLiquido = salarioBase;
-}
-console.log(salarioBase);
-console.log(salarioLiquido);
