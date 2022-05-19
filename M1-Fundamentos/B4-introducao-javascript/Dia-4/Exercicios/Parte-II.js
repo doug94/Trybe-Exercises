971e03630054f69ba4f67c35ef3e2c439099bb93
Douglas Alves <douglas_dca@hotmail.com>
2022-05-18 21:49:41 -0300

    Exercícios de funções concluídos

--- M1-Fundamentos/B4-introducao-javascript/Dia-4/Exercicios/Parte-II.js
+++ M1-Fundamentos/B4-introducao-javascript/Dia-4/Exercicios/Parte-II.js
@@ -37,3 +37,44 @@ function encontraMenorValor(arr) {
     return indice;
 }
 
+//5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+function contaMaisRepetidos(arr){
+    let mostRepeatedCount = 0;
+    let mostRepeatedValue;
+    let repeated = 0;
+    let repeatedValue;
+    for (let item of arr) {
+      for (let i = 0; i < arr.length; i += 1) {
+        if (item === arr[i]) {
+          repeated += 1;
+          repeatedValue = arr[i];
+        }
+      }
+      if (repeated > mostRepeatedCount) {
+        mostRepeatedCount = repeated;
+        mostRepeatedValue = repeatedValue;
+        repeated = 0;
+      } else {
+        repeated = 0;
+      }
+    }
+    return mostRepeatedValue;
+}
+
+//6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+function somaNumerosNaturais(numero) {
+    if (numero >= 0) {
+        let soma = 0;
+        for (let i = 0; i <= numero; i += 1) {
+            soma += i;
+        }
+        return soma;
+    }
+    return 0;
+}
+
+function verificaFimPalavra(word, ending) {
+    return word.endsWith(ending);
+}
+
+console.log(verificaFimPalavra("joaofernando", "fernan"));
