0cd5232b55d2ac07b7e3b94485c49a3f5423d7da
Douglas Alves <douglas_dca@hotmail.com>
2022-05-19 13:58:43 -0300

    Exercício bônus números romanos iniciado

---
+++ M1-Fundamentos/B4-introducao-javascript/Dia-4/Exercicios/Bonus.js
@@ -0,0 +1,12 @@
+// Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa. 
+function converteNumeroRomanoEmNatural(numeroRomano) {
+    let algarismosRomanos = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    }
+}
