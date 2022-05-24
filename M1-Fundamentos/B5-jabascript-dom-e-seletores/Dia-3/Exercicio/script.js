5c7352825521d734e633afd4233f70c05f7f2f03
Douglas Alves <douglas_dca@hotmail.com>
2022-05-24 15:02:57 -0300

    Exercício 3 concluído

--- M1-Fundamentos/B5-jabascript-dom-e-seletores/Dia-3/Exercicio/script.js
+++ M1-Fundamentos/B5-jabascript-dom-e-seletores/Dia-3/Exercicio/script.js
@@ -46,22 +46,32 @@ buttonHolidays("Feriados");
 
 function highlightHolidays() {
     const holidays = document.getElementsByClassName("holiday");
-    const bgcolor = holidays[0].style.backgroundColor;
-    if (bgColor !== "Red") {
-        document.getElementById("btn-holiday").addEventListener("click", function () {
-            const holidays = document.getElementsByClassName("holiday");
-            for (let index = 0; index < holidays.length; index += 1) {
+    document.getElementById("btn-holiday").addEventListener("click", function() {
+        const holidays = document.getElementsByClassName("holiday");
+        for (let index = 0; index < holidays.length; index += 1) {
+            if (holidays[index].style.backgroundColor === '') {
                 holidays[index].style.backgroundColor = "Red";
+            } else {
+                holidays[index].style.backgroundColor = '';
             }
-        });
-    } else {
-        document.getElementById("btn-holiday").addEventListener("click", function () {
-            const holidays = document.getElementsByClassName("holiday");
-            for (let index = 0; index < holidays.length; index += 1) {
-                holidays[index].style.backgroundColor = "White";
-            }
-        });
-    }
+        }
+    });
+    // const bgcolor = holidays[0].style.backgroundColor;
+    // if (bgColor !== "Red") {
+    //     document.getElementById("btn-holiday").addEventListener("click", function () {
+    //         const holidays = document.getElementsByClassName("holiday");
+    //         for (let index = 0; index < holidays.length; index += 1) {
+    //             holidays[index].style.backgroundColor = "Red";
+    //         }
+    //     });
+    // } else {
+    //     document.getElementById("btn-holiday").addEventListener("click", function () {
+    //         const holidays = document.getElementsByClassName("holiday");
+    //         for (let index = 0; index < holidays.length; index += 1) {
+    //             holidays[index].style.backgroundColor = "White";
+    //         }
+    //     });
+    // }
 }
 
 highlightHolidays();
