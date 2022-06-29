9b44ce0b3602328e2975a57695926cf5b2ba305e
Douglas Alves <douglas_dca@hotmail.com>
2022-06-28 23:51:41 -0300

    Conteúdo de fixação concluído

--- M1-Fundamentos/B8-higher-order-functions/Dia-5/Conteudo/ArrayDestructuring.js
+++ M1-Fundamentos/B8-higher-order-functions/Dia-5/Conteudo/ArrayDestructuring.js
@@ -19,6 +19,9 @@ console.log(comida, animal, bebida); // arroz gato água
 
 let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 
-console.log(numerosPares); // [6, 8, 10, 12];
 
 // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+[, , , ...pares] = numerosPares;
+
+console.log(pares); // [6, 8, 10, 12];
