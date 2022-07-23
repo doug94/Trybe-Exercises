cfacdaf69efdbbc94e355a87b1ee4c8525931bdf
Douglas Alves <douglas_dca@hotmail.com>
2022-07-23 00:09:32 -0300

    captura de eventos parou de funcionar na textarea com event handler genérico

--- M2-Front-End/B11-componentes-com-estado/Dia-2/react-form/src/Form.js
+++ M2-Front-End/B11-componentes-com-estado/Dia-2/react-form/src/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Estados from './Estados';
 
 class Form extends Component {
   constructor() {
@@ -8,14 +9,17 @@ class Form extends Component {
 
     this.state = {
       estadoFavorito: '',
+      email: '',
+      nome: '',
+      idade: 0,
+      vaiComparecer: false
     };
   }
 
-
-  handleChange(event) {
-    this.setState({
-      estadoFavorito: event.target.value,
-    });
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value })
   }
 
   render() {
@@ -23,18 +27,55 @@ class Form extends Component {
       <div>
         <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
         <form className="form">
+
           <label>
             Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
-              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+            <textarea name="estadoFavorito" placeholder="Digite o motivo da sua escolha" />
           </label>
-          <input
-            type="number"
-            name="idade"
-          />
-          <input
-            type="checkbox"
-            name="vaiComparecer"
-          />
+
+          <label>
+            Escolha seu estado
+            <Estados value={this.handleChange} />
+          </label>
+
+          <label>
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="Digite seu email"
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label>
+            Nome
+            <input
+              name="nome"
+              placeholder="Digite seu nome"
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label>
+            Idade
+            <input
+              type="number"
+              name="idade"
+              placeholder='Digite sua idade'
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label>
+            Vai comparecer
+            <input
+              type="checkbox"
+              name="vaiComparecer"
+              onChange={this.handleChange}
+            />
+          </label>
+
         </form>
       </div>
     );
