0ace47aaf66ab0a4e1daaa0ea2a1d8314a5b4b71
Douglas Alves <douglas_dca@hotmail.com>
2022-11-03 16:44:04 -0300

    Conteúdo de fixação concluído

---
+++ M3-Back-End/B22-introducao-node-js/Dia-4/soccer-team-manager/src/middlewares/validateTeam.js
@@ -0,0 +1,7 @@
+const validadeTeam = (req, res, next) => {
+  const requiredProperties = ['nome', 'sigla'];
+  if (requiredProperties.every((property) => property in req.body)) next();
+  else res.sendStatus(400);
+};
+
+module.exports = validadeTeam;
