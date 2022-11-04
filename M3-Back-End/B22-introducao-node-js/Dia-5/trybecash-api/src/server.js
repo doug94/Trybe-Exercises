4c0dd618681f4984b54ce4d3d3d69e16cd19ae03
Douglas Alves <douglas_dca@hotmail.com>
2022-11-04 00:52:54 -0300

    Implementação da aplicação concluída

---
+++ M3-Back-End/B22-introducao-node-js/Dia-5/trybecash-api/src/server.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+const connection = require('./db/connection');
+
+const port = 3001;
+
+app.listen(port, async () => {
+  console.log(`A API Trybecash está sendo executada na porta ${port}`);
+  const [result] = await connection.execute('SELECT 1');
+  if (result) console.log('MySQL connection OK');
+})
